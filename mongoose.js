36e52befba20abd07c616dd8dc54b15373ca3b8c
Rachel Arkebauer <rarkebauer@gmail.com>
2017-05-03 21:57:22 -0400

    integrated mongodb collection with mLab so app can be deployed on heroku

--- mongoose.js
+++ mongoose.js
@@ -5,7 +5,18 @@ mongoose.Promise = global.Promise; //set mongoose promise to the globle promise 
 
 var Schema = mongoose.Schema;
 
-mongoose.connect('mongodb://localhost:27017/urlShortener'); 
+//mongoose.connect('mongodb://localhost:27017/urlShortener'); 
+var url = process.env.MONGOLAB_URI;
+//var url = 'mongodb://localhost:27017/urlShortener';
+/*mongoose.connect(url, function (err, db) {
+	if(err) {
+		console.log('Unable to connect to the mongoDB server. Error:', err)
+	} else {
+		console.log('Connection established to', url);
+	}
+}); */
+mongoose.connect(url);
+console.log(url);
 
 
 var urlSchema = new Schema({ //create schema
@@ -17,4 +28,9 @@ const UrlEntry = mongoose.model('UrlEntry', urlSchema) //create a model
 
 //exports.UrlEntry = UrlEntry;
 
+
 module.exports = UrlEntry;
+
+
+
+//export MONGOLAB_URI="mongodb://rarkebauer:herocupid23@ds131151.mlab.com:31151/urls"
