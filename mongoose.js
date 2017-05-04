2367435030a4ede125fab0fa74b32da648e3413b
Rachel Arkebauer <rarkebauer@gmail.com>
2017-05-04 07:47:47 -0400

    updated security

---
+++ mongoose.js
@@ -0,0 +1,32 @@
+
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise; //set mongoose promise to the globle promise variable found in ES6
+
+var Schema = mongoose.Schema;
+
+//mongoose.connect('mongodb://localhost:27017/urlShortener'); 
+var url = process.env.MONGOLAB_URI;
+//var url = 'mongodb://localhost:27017/urlShortener';
+/*mongoose.connect(url, function (err, db) {
+	if(err) {
+		console.log('Unable to connect to the mongoDB server. Error:', err)
+	} else {
+		console.log('Connection established to', url);
+	}
+}); */
+mongoose.connect(url);
+
+
+var urlSchema = new Schema({ //create schema
+	original: { type: String, required: true },
+	shortCode: { type: String, required: true }
+});
+
+const UrlEntry = mongoose.model('UrlEntry', urlSchema) //create a model
+
+//exports.UrlEntry = UrlEntry;
+
+
+module.exports = UrlEntry;
+
